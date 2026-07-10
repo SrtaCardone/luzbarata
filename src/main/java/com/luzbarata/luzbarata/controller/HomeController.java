ce9ba3b5b3bf644356188b731945b4daa631df21
Señorita <Silviagutierrezcardona@outlook.es>
2026-07-10 10:32:21 +0200

    Guardar suscriptores en MySQL mediante Spring Data JPA

--- src/main/java/com/luzbarata/luzbarata/controller/HomeController.java
+++ src/main/java/com/luzbarata/luzbarata/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.luzbarata.luzbarata.controller;
-
+import com.luzbarata.luzbarata.entity.Subscriber;
+import com.luzbarata.luzbarata.repository.SubscriberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    private final SubscriberRepository subscriberRepository;
+
+public HomeController(SubscriberRepository subscriberRepository) {
+    this.subscriberRepository = subscriberRepository;
+}
 
     @GetMapping("/")
     public String home() {
@@ -16,7 +22,8 @@ public class HomeController {
 
     @PostMapping("/suscribirse")
     public String suscribirse(@RequestParam String email, Model model) {
-
+        Subscriber subscriber = new Subscriber(email);
+        subscriberRepository.save(subscriber);
         model.addAttribute("email", email);
 
         return "confirmacion";
